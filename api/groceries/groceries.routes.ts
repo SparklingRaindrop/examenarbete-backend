cab1c748d51c25deb2895d898b7da2a59fba46d1
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-03 15:54:57 +0100

    Add "add" route on "/groceries"

--- api/groceries/groceries.routes.ts
+++ api/groceries/groceries.routes.ts
@@ -1,9 +1,10 @@
-import { removeItem, getItems } from "./groceries.controllers";
+import { removeItem, getItems, addItem } from "./groceries.controllers";
 import express from 'express';
 
 const route = express.Router();
 
 route.get('/', getItems);
 route.delete('/:itemId', removeItem);
+route.post('/', addItem);
 
 export { route as groceriesRoute };
