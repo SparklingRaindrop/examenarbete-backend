fa993d28c0a0f6bb5377327891942a959355d3f1
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-03 17:01:03 +0100

    Add getById function and fix bug

--- api/groceries/groceries.model.ts
+++ api/groceries/groceries.model.ts
@@ -1,10 +1,15 @@
 import knex from '../../knex/knex';
 import { Grocery, GroceryItem } from '../../types/grocery';
 
-export async function getAll(): Promise<Item[] | undefined> {
+interface newData {
+    itemId: string,
+    amount: number
+};
+
+export async function getAll(): Promise<Grocery | undefined> {
     try {
         return await knex<Grocery>('Item')
-            .select('Item.*')
+            .select('Item.*', 'Grocery.amount')
             .innerJoin(
                 'Grocery',
                 'item_id',
@@ -18,7 +23,19 @@ export async function getAll(): Promise<Item[] | undefined> {
     return;
 }
 
-export async function remove(itemId: string): Promise<Item[] | undefined> {
+export async function getById(itemId: string): Promise<GroceryItem | undefined> {
+    try {
+        return knex<GroceryItem>('Grocery')
+            .select('*')
+            .where('item_id', itemId)
+            .first();
+    } catch (error) {
+        console.error(error);
+    }
+    return;
+}
+
+export async function remove(itemId: string): Promise<void | undefined> {
     try {
         await knex<Grocery>('Grocery')
             .where('item_id', itemId)
@@ -29,15 +46,27 @@ export async function remove(itemId: string): Promise<Item[] | undefined> {
     return;
 }
 
-export async function add(itemId: string): Promise<void> {
-    const newData: GroceryItem = {
-        item_id: itemId,
+export async function add(newData: newData): Promise<void> {
+    const data: GroceryItem = {
+        item_id: newData.itemId,
+        amount: newData.amount,
         updated_at: new Date()
     };
 
     try {
         await knex<GroceryItem>('Grocery')
-            .insert(newData);
+            .insert(data);
+    } catch (error) {
+        console.error(error);
+    }
+    return;
+}
+
+export async function edit(newData: newData): Promise<void> {
+    try {
+        await knex<GroceryItem>('Grocery')
+            .where('item_id', newData.itemId)
+            .update({ amount: newData.amount });
     } catch (error) {
         console.error(error);
     }
--- types/grocery.d.ts
+++ types/grocery.d.ts
@@ -1,6 +1,8 @@
 export interface GroceryItem {
     item_id: string;
     updated_at: Date;
-}
+    amount: number;
+};
 
 export type Grocery = GroceryItem[];
+
