6911d22f43a7c13e8446df4ef016cf8514fe196c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 12:40:36 +0100

    Add enum for error messages

--- types/api.d.ts
+++ types/api.d.ts
@@ -3,4 +3,10 @@ export const enum Status {
     Created = 201,
     NoContent = 204,
     BadRequest = 400,
+    NotFound = 404,
+}
+
+export const enum Error {
+    MissingData = 'Required data is missing.',
+    AlreadyExist = 'The requested item already exists.',
 }
