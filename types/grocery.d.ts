8a2702a5a995e1e7f29c440ad235ea0ee59b7009
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 14:21:23 +0100

    Move type from separated file

--- types/grocery.d.ts
+++
@@ -1,9 +0,0 @@
-export interface GroceryItem {
-    item_id: string;
-    updated_at: Date;
-    amount: number;
-    isChecked: boolean | number;
-}
-
-export type Grocery = GroceryItem[];
-
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,19 @@
+interface User {
+    id: string;
+    username: string;
+    email: string;
+}
+
+interface Grocery {
+    id: string;
+    item_id: Pick<Item, 'id'>;
+    updated_at: Date;
+    amount: number;
+    isChecked: boolean | number;
+}
+
 interface Item {
     id: string;
     name: string;
+    user_id?: Pick<User, 'id'>;
 }
