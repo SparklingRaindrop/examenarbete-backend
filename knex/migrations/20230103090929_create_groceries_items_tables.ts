9be9c9ff8e331bd528548c1d4db0188a54245eb9
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-11 10:44:42 +0100

    Add user_id to tables

--- knex/migrations/20230103090929_create_groceries_items_tables.ts
+++ knex/migrations/20230103090929_create_groceries_items_tables.ts
@@ -12,14 +12,17 @@ export async function up(knex: Knex): Promise<void> {
             table.string('id').notNullable().primary();
             table.string('name').notNullable();
             table.string('user_id');
+            table.foreign('user_id').references('User.id');
         })
         .createTable('Grocery', function (table) {
             table.string('id').notNullable().primary();
             table.timestamp('updated_at');
             table.float('amount').notNullable();
-            table.string('item_id').notNullable();
             table.boolean('isChecked').notNullable();
+            table.string('item_id').notNullable();
+            table.string('user_id').notNullable();
             table.foreign('item_id').references('Item.id');
+            table.foreign('user_id').references('User.id');
         });
 }
 
