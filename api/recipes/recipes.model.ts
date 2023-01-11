6a5bb1995dfbfb94bc7271b200b6f02ba9ecd0c1
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-11 16:42:14 +0100

    Add getRecipeById

--- api/recipes/recipes.model.ts
+++ api/recipes/recipes.model.ts
@@ -1,7 +1,15 @@
 import knex from '../../knex/knex';
 
-export function getRecipes(userId: Pick<User, 'id'>): Promise<Recipe[] | undefined> {
+export function getRecipes(userId: Pick<User, 'id'>): Promise<Recipe[]> {
     return knex<Recipe>('Recipe')
+        .where('user_id', null)
+        .orWhere('user_id', userId)
+        .select('Recipe.id', 'Recipe.title');
+}
+
+export function getRecipesById(userId: Pick<User, 'id'>, recipe_id: Pick<Recipe, 'id'>): Promise<Recipe[]> {
+    return knex<Recipe>('Recipe')
+        .where('id', recipe_id)
         .where('user_id', null)
         .orWhere('user_id', userId)
         .select('Recipe.id', 'Recipe.title');
