54f5c1bd0c654465976b4dc1441d49eb46677c13
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-12 12:49:16 +0100

    Add isAvailable function

--- api/units/units.model.ts
+++ api/units/units.model.ts
@@ -1,15 +1,29 @@
 import knex from '../../knex/knex';
 
-export async function getUnit(userId: User['id'], unitId: Unit['id']): Promise<Item | undefined> {
+export async function getUnit(userId: User['id'], unitId: Unit['id']): Promise<Unit | undefined> {
     try {
         return knex<Item>('Item')
             .where('id', unitId)
-            .where('user_id', null)
-            .orWhere('user_id', userId)
+            .andWhere(builder =>
+                builder
+                    .where('Item.user_id', userId)
+                    .orWhere('Item.user_id', null)
+            )
             .select('id', 'name')
             .first();
     } catch (error) {
         console.error(error);
         return;
     }
+}
+
+export async function isAvailableUnit(userId: User['id'], unitId: Unit['id']): Promise<boolean> {
+    return knex<Unit>('Unit')
+        .where('id', unitId)
+        .andWhere(builder =>
+            builder
+                .where('user_id', userId)
+                .orWhere('user_id', null)
+        )
+        .then((result) => result.length !== 0);
 }
