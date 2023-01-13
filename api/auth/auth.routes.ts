20433a0518e022ea6d8ab74d01e9d85d1a0bf688
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-13 10:52:51 +0100

    Add logout on auth route

--- api/auth/auth.routes.ts
+++ api/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { checkToken } from '../../middleware/auth';
-import { createNewUser, login, remove, update } from './auth.controller';
+import { createNewUser, login, logout, remove, update } from './auth.controller';
 
 const route = express.Router();
 
 route.post('/login', login);
+route.post('/logout', logout);
 route.post('/user', createNewUser);
 route.delete('/user', checkToken, remove);
 route.patch('/user', checkToken, update);
