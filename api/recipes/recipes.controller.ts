13c39acf2c1f5a82e703556df00efcda5bb993ea
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-19 15:37:02 +0100

    Improve response data

--- api/recipes/recipes.controller.ts
+++ api/recipes/recipes.controller.ts
@@ -15,34 +15,7 @@ export async function getAll(req: Request, res: Response, next: NextFunction): P
         return;
     }
 
-    let result = recipes as Array<Recipe & {
-        category?: Omit<CategoryList, 'recipe_id'>[],
-        ingredients?: Ingredient[];
-    }>;
-
-    // Adding category
-    /* const categoryList = await getCategories(id);
-    if (categoryList) {
-        result = result.map(recipe => {
-            const { id: recipeId } = recipe;
-            const category = categoryList.filter(category =>
-                category.recipe_id === recipeId
-            );
-            category.forEach(c => delete c.recipe_id);
-            recipe.category = category;
-
-            return recipe;
-        });
-    } */
-
-    // Adding ingredients
-    result = await Promise.all(result.map(async (recipe) => {
-        const ingredients = await getIngredients(id, recipe.id).catch((err) => next(err));
-        recipe.ingredients = ingredients ? ingredients : [];
-        return recipe;
-    }));
-
-    res.send(result);
+    res.send(recipes);
 }
 
 export async function getOne(req: Request, res: Response, next: NextFunction): Promise<void> {
--- api/recipes/recipes.model.ts
+++ api/recipes/recipes.model.ts
@@ -1,6 +1,6 @@
 import knex from '../../knex/knex';
 
-export function getRecipes(userId: User['id'], keyword?: string): Promise<Omit<Recipe, 'user_id'>[]> {
+export async function getRecipes(userId: User['id'], keyword?: string): Promise<{ [key: string]: string }[]> {
     return knex<Recipe>('Recipe')
         .andWhere(builder =>
             builder
@@ -12,7 +12,43 @@ export function getRecipes(userId: User['id'], keyword?: string): Promise<Omit<R
                 queryBuilder.where('title', 'like', `%${keyword}%`);
             }
         })
-        .select('id', 'title');
+        .leftJoin(
+            'Ingredient',
+            'Recipe.id',
+            'Ingredient.recipe_id'
+        )
+        .leftJoin(
+            'Item',
+            'Ingredient.item_id',
+            'Item.id'
+        )
+        .leftJoin(
+            'Unit',
+            'Item.unit_id',
+            'Unit.id'
+        )
+        .select(
+            'Recipe.id', 'Recipe.title',
+            'Ingredient.amount',
+            'Item.name as item_name', 'Item.id as item_id',
+            'Unit.id as unit_id', 'Unit.name as unit_name'
+        )
+        .then((result) => (
+            result.map((item: any) => {
+                const newItem = { ...item };
+                for (const key in newItem) {
+                    if (!key.includes('_') || key === 'updated_at') continue;
+
+                    const [property, subProperty] = key.split('_');
+                    newItem[property] = {
+                        ...newItem[property],
+                        [subProperty]: newItem[key]
+                    };
+                    delete newItem[key];
+                }
+                return newItem;
+            })
+        ));
 }
 
 export function getRecipe(userId: User['id'], recipe_id: Recipe['id']): Promise<Omit<Recipe, 'user_id'>> {
