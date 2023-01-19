8a24aa9ae2b236038ceafd3c5e9a7aa983f72aad
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-19 18:19:45 +0100

    Revert

--- api/plans/plans.model.ts
+++ api/plans/plans.model.ts
@@ -7,39 +7,14 @@ const currentMonth = {
 };
 
 // range is set to current month by default
-export async function getPlans(userId: User['id'], range: { from: Date | null, to: Date | null } = currentMonth): Promise<Plan[]> {
+export function getPlans(userId: User['id'], range: { from: Date | null, to: Date | null } = currentMonth): Promise<Plan[]> {
     return knex<Plan>('Plan')
-        .where('Plan.user_id', userId)
+        .where('user_id', userId)
         .whereBetween('date', [
             range.from ? range.from.getTime() : currentMonth.from.getTime(),
             range.to ? range.to.getTime() : currentMonth.to.getTime()
         ])
-        .leftJoin(
-            'Recipes',
-            'recipe_id',
-            '=',
-            'Recipe.id'
-        )
-        .select(
-            'Plan.id', 'updated_at', 'date', 'type',
-            'Recipe.recipe_id', 'title as recipe_title'
-        )
-        .then((result) => (
-            result.map(item => {
-                const newItem = { ...item };
-                for (const key in newItem) {
-                    if (!key.includes('_') || key === 'updated_at') continue;
-
-                    const [property, subProperty] = key.split('_');
-                    newItem[property] = {
-                        ...newItem[property],
-                        [subProperty]: newItem[key]
-                    };
-                    delete newItem[key];
-                }
-                return newItem;
-            })
-        ));
+        .select('id', 'updated_at', 'date', 'type', 'recipe_id');
 }
 
 export function getPlan(userId: User['id'], planId: Plan['id']): Promise<Plan> {
