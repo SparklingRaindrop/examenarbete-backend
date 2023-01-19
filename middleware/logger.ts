473b56edc1a617f31965edebfb685014f44677d6
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-19 13:25:17 +0100

    Add status code

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-    console.info('\x1b[34m%s\x1b[0m', `Request On:"${req.originalUrl}" | Request Type: ${req.method} | time: ${Date.now()}`);
+    res.on('finish', () => {
+        console.info('\x1b[34m%s\x1b[0m',
+            `Request On:"${req.originalUrl}" | Request Type: ${req.method} | Status: ${res.statusCode} | time: ${Date.now()}`
+        );
+    });
     next();
 }
