2d01226f89b4455d63123497fec2e880316aae9c
Tsubasa <92432492+SparklingRaindrop@users.noreply.github.com>
2023-01-28 16:00:03 +0100

    Revert "Develop"

--- knex/knex.ts
+++
@@ -1,5 +0,0 @@
-import knex from 'knex';
-const environment = process.env.NODE_ENV || 'development'; // eslint-disable-line
-import config from '../knexfile';
-const knexInstance = knex(config[environment]);
-export default knexInstance;
--- types/error.ts
+++
@@ -1,8 +0,0 @@
-enum Error {
-    MissingData = 'Required data is missing.',
-    AlreadyExist = 'The requested item already exists.',
-    SomethingHappened = 'Something happened on server',
-    InvalidDataType = 'Received invalid type of data.',
-}
-
-export default Error;
